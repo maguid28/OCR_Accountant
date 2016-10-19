7c94d7603a1c434d95a4110f74711b2f74535656
Daniel Maguire <daniel.maguire28@mail.dcu.ie>
2016-10-19 16:42:39 +0100

    Added OCRScan Activity Configured .gitignore file correctly

---
+++ src/OCRAccountingApp/app/src/main/java/com/finalyearproject/dan/ocraccountingapp/OCRScan.java
@@ -0,0 +1,15 @@
+package com.finalyearproject.dan.ocraccountingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static com.finalyearproject.dan.ocraccountingapp.R.styleable.View;
+
+public class OCRScan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ocrscan);
+    }
+}
