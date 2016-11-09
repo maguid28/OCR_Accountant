4955d318018b53d3dfef5340d32dccc08739d0eb
Daniel Maguire <daniel.maguire28@mail.dcu.ie>
2016-11-09 21:53:23 +0000

    Changed tess-two as gradle dependency

--- src/OCRAccountingApp/app/src/main/java/com/finalyearproject/dan/ocraccountingapp/OCRScan.java
+++ src/OCRAccountingApp/app/src/main/java/com/finalyearproject/dan/ocraccountingapp/OCRScan.java
@@ -3,7 +3,6 @@ package com.finalyearproject.dan.ocraccountingapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import static com.finalyearproject.dan.ocraccountingapp.R.styleable.View;
 
 public class OCRScan extends AppCompatActivity {
 
