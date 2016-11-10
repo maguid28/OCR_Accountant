d856e84a1dec181e5eed4175d26b72501c056fa9
Daniel Maguire <daniel.maguire28@mail.dcu.ie>
2016-11-10 02:28:40 +0000

    First successful OCR implementation.

--- src/OCRAccountingApp/app/src/main/java/com/finalyearproject/dan/ocraccountingapp/OCRScan.java
+++ src/OCRAccountingApp/app/src/main/java/com/finalyearproject/dan/ocraccountingapp/OCRScan.java
@@ -1,14 +1,104 @@
 package com.finalyearproject.dan.ocraccountingapp;
 
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import android.view.View;
 
 
 public class OCRScan extends AppCompatActivity {
+
+    Bitmap image;
+    private TessBaseAPI mTess;
+    String datapath = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ocrscan);
+
+        //init image
+        image = BitmapFactory.decodeResource(getResources(), R.drawable.test2);
+        datapath = getFilesDir() + "/tesseract/";
+
+        //make sure training data has been copied
+        checkFile(new File(datapath + "tessdata/"));
+
+        //init Tesseract API
+        String lang = "eng";
+
+        mTess = new TessBaseAPI();
+        mTess.init(datapath, lang);
     }
+
+
+
+    //copy training data to device
+    private void copyFile() {
+        try {
+            //location we want to file to be at
+            String filepath = datapath + "/tessdata/eng.traineddata";
+
+            //get access to AssetManager
+            AssetManager assetManager = getAssets();
+
+            //open byte streams for reading and writing
+            InputStream instream = assetManager.open("tessdata/eng.traineddata");
+            OutputStream outstream = new FileOutputStream(filepath);
+
+            //copy the file to the location specified by filepath
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = instream.read(buffer)) != -1) {
+                outstream.write(buffer, 0, read);
+            }
+            outstream.flush();
+            outstream.close();
+            instream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    //check whether file is on device
+    private void checkFile(File dir) {
+        //directory does not exist, but we can create it
+        if(!dir.exists() && dir.mkdirs()) {
+            copyFile();
+        }
+        if(dir.exists()) {
+            String datafilepath = datapath + "/tessdata/eng.traineddata";
+            File datafile = new File(datafilepath);
+            if(!datafile.exists()) {
+                copyFile();
+            }
+        }
+    }
+
+    public void processImage(View v) {
+        mTess.setImage(image);
+        String OCRresult = mTess.getUTF8Text();
+        TextView OCRTextView = (TextView) findViewById(R.id.OCRTextView);
+        OCRTextView.setText(OCRresult);
+    }
+
+
+
 }
